e1171fc2532ec6dfaa9bd5cccaf46954ed661301
Maxime Bouyges <maxime.bouyges@gmail.com>
2023-09-12 15:07:39 +0200

    removing dummy type

--- src/ParametricFilenames.jl
+++ src/ParametricFilenames.jl
@@ -3,28 +3,18 @@ using DataFrames
 using Random
 using CSV
 
-export ParametricFilename, exists, new_filename, get_filename, new_parameter!, get_dataframe
-
-const _PREFIX = :prefix
+# Constants
+const _PREFIX = "prefix"
 const _IDENTIFIER = :identifier
-const _EXTENSION = :extension
+const _EXTENSION = "extension"
 const _DEFAULT_NRANDOM = 4
+const _NRANDOM = "nrandom"
+const _WRITE_PATH = "write_path"
 
-"""
-Almost just a wrapper to `DataFrame`. Could be declared as <: AbstractDataFrame
-"""
-struct ParametricFilename
-    df::DataFrame
-    prefix::String
-    extension::String
-    nrandom::Int
-    write_path::String # path to write (automatically) the DataFrame
-end
-_get_nrandom(pf::ParametricFilename) = pf.nrandom
-get_dataframe(pf::ParametricFilename) = pf.df
-_get_prefix(pf::ParametricFilename) = pf.prefix
-_get_extension(pf::ParametricFilename) = pf.extension
-_get_write_path(pf::ParametricFilename) = pf.write_path
+_get_nrandom(df::DataFrame) = metadata(df, _NRANDOM)
+_get_prefix(df::DataFrame) = metadata(df, _PREFIX)
+_get_extension(df::DataFrame) = metadata(df, _EXTENSION)
+_get_write_path(df::DataFrame) = metadata(df, _WRITE_PATH)
 
 """
 `colnames` and `coltypes` must be provided as iterables (`Tuple`, `Vector`, ...) of `Symbol`.
@@ -40,30 +30,36 @@ function ParametricFilename(
     d = Dict(name => type[] for (name, type) in zip(colnames, coltypes))
     d[_IDENTIFIER] = String[]
     df = DataFrame(d)
-    return ParametricFilename(df, prefix, extension, nrandom, write_path)
+
+    metadata!(df, _PREFIX, prefix, style = :note)
+    metadata!(df, _EXTENSION, extension, style = :note)
+    metadata!(df, _NRANDOM, nrandom, style = :note)
+    metadata!(df, _WRITE_PATH, write_path, style = :note)
+
+    return df
 end
 
 """
-Build a new random String identifier according to the number of chars of the `ParametricFilename`
+Build a new random String identifier according to the number of chars of the `DataFrame`
 """
-_new_identifier(pf::ParametricFilename) = lowercase(randstring(_get_nrandom(pf)))
+_new_identifier(df::DataFrame) = lowercase(randstring(_get_nrandom(df)))
 
 """
 Tell if the set of parameters values already exist in the database or not.
 `t` is a `NamedTuple` of a `Dict`
 """
-function exists(pf::ParametricFilename, t)
+function exists(df::DataFrame, t)
     query = [name => x -> x .== value for (name, value) in pairs(t)]
-    return nrow(subset(get_dataframe(pf), query)) > 0
+    return nrow(subset(df, query)) > 0
 end
 
 """
 Build a filename from the String identifier and, eventually, the prefix and extension
-of the `ParametricFilename`.
+of the `DataFrame`.
 """
-function _build_filename(pf::ParametricFilename, id)
-    filename = _get_prefix(pf) * id
-    ext = _get_extension(pf)
+function _build_filename(df::DataFrame, id)
+    filename = _get_prefix(df) * id
+    ext = _get_extension(df)
     return length(ext) > 0 ? filename * "." * ext : filename
 end
 
@@ -71,10 +67,10 @@ end
 Build a new (unique) filename based on the input parameter values.
 `t` is a `NamedTuple` of a `Dict`
 """
-function new_filename(pf::ParametricFilename, t)
-    @assert !exists(pf, t) "New entry already exists"
+function new_filename(df::DataFrame, t)
+    @assert !exists(df, t) "New entry already exists"
 
-    id = _new_identifier(pf)
+    id = _new_identifier(df)
     if t isa NamedTuple
         # Oups, I can't use `_t = (t..., _IDENTIFIER = id)` here,
         # beause _IDENTIFIER is not interpreted, it because the name...
@@ -87,14 +83,14 @@ function new_filename(pf::ParametricFilename, t)
     end
 
     # Add to dataframe
-    push!(get_dataframe(pf), _t)
+    push!(df, _t)
 
     # Write dataframe to file if asked
-    wpath = _get_write_path(pf)
-    (length(wpath) > 0) && write(pf, wpath)
+    wpath = _get_write_path(df)
+    (length(wpath) > 0) && write(df, wpath)
 
     # Return new name
-    return _build_filename(pf, id)
+    return _build_filename(df, id)
 end
 
 """
@@ -103,14 +99,14 @@ if this value was preexistent to the function call. If the filename does not
 exist yet, it is created (and added to the database).
 `t` is a `NamedTuple` of a `Dict`
 """
-function get_filename(pf::ParametricFilename, t)
+function get_filename(df::DataFrame, t)
     query = [name => x -> x .== value for (name, value) in pairs(t)]
-    _df = subset(get_dataframe(pf), query)
+    _df = subset(df, query)
     if nrow(_df) > 0
         id = _df[1, _IDENTIFIER]
-        return _build_filename(pf, id), false
+        return _build_filename(df, id), false
     else
-        return new_filename(pf, t), true
+        return new_filename(df, t), true
     end
 end
 
@@ -118,25 +114,18 @@ end
 `val` is a unique value (default value) or a vector of values
 (one for each row)
 """
-function new_parameter!(pf::ParametricFilename, name, value)
-    insertcols!(get_dataframe(pf), name => value)
-end
+new_parameter!(df::DataFrame, name, value) = insertcols!(df, name => value)
 
-function read(
-    path;
-    prefix = "",
-    extension = "",
-    nrandom = _DEFAULT_NRANDOM,
-    autowrite = false,
-)
+function read(path; autowrite = false)
     df = CSV.read(path, DataFrame)
     wpath = autowrite ? path : ""
-    return ParametricFilename(df, prefix, extension, nrandom, wpath)
+    metadata!(df, _WRITE_PATH, wpath)
+    return df
 end
 
-function write(pf::ParametricFilename, path = "")
-    _wpath = length(path) > 0 ? path : _get_write_path(pf)
-    CSV.write(_wpath, get_dataframe(pf))
+function write(df::DataFrame, path = "")
+    _wpath = length(path) > 0 ? path : _get_write_path(df)
+    CSV.write(_wpath, df)
 end
 
 macro only_if_new(df, filename, f)
@@ -149,4 +138,4 @@ macro only_if_new(df, filename, f)
     end
 end
 
-end # module ParametricFilenames
+end
